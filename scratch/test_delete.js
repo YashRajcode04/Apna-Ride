7267531e25571ebd3cdf53fa38150bf323052ec2
Yash Raj <yashrajlfs@gmail.com>
2026-05-03 16:13:21 +0530

    Configure project for Vercel deployment: Added vercel.json and root package.json

---
+++ scratch/test_delete.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const testDelete = async () => {
+  const baseURL = 'http://localhost:5000/api';
+  
+  // Need a token
+  // Since I can't easily get a token here, I'll just check the code more carefully.
+  console.log('Testing delete logic...');
+};
+
+testDelete();
